5108a29f4ebc30a2ea9fcf4b72db587cd8aa9a56
octaltree <octaltree@gmail.com>
2016-03-15 17:41:26 +0900

    routing

--- metaserver2/src/Routing.hs
+++ metaserver2/src/Routing.hs
@@ -1,1 +1,32 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
 module Routing where
+
+import Models
+import Servant
+
+type BookEP = "books" :> Get '[JSON] [Book]
+  :<|> "books" :> ReqBody '[JSON] Book :> Post '[JSON] Book
+  :<|> "books" :> Capture "id" Int :> Get '[JSON] Book
+  :<|> "books" :> Capture "id" Int :> ReqBody '[JSON] Book :> Put '[JSON] ()
+  :<|> "books" :> Capture "id" Int :> Delete '[JSON] ()
+
+type CircleEP = "circles" :> Get '[JSON] [Circle]
+  :<|> "circles" :> ReqBody '[JSON] Circle :> Post '[JSON] Circle
+  :<|> "circles" :> Capture "id" Int :> Get '[JSON] Circle
+  :<|> "circles" :> Capture "id" Int :> ReqBody '[JSON] Circle :> Put '[JSON] ()
+  :<|> "circles" :> Capture "id" Int :> Delete '[JSON] ()
+
+type WriterEP = "writers" :> Get '[JSON] [Writer]
+  :<|> "writers" :> ReqBody '[JSON] Writer :> Post '[JSON] Writer
+  :<|> "writers" :> Capture "id" Int :> Get '[JSON] Writer
+  :<|> "writers" :> Capture "id" Int :> ReqBody '[JSON] Writer :> Put '[JSON] ()
+  :<|> "writers" :> Capture "id" Int :> Delete '[JSON] ()
+
+type TagEP = "tags" :> Get '[JSON] [String]
+
+type API = BookEP
+  :<|> TagEP
+  :<|> CircleEP
+  :<|> WriterEP
