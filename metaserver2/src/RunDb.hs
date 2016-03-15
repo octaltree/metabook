33cffece61e29952ce70bddee6ba1e53aaf7b1cb
octaltree <octaltree@gmail.com>
2016-03-15 22:46:25 +0900

    CR return a

--- metaserver2/src/RunDb.hs
+++ metaserver2/src/RunDb.hs
@@ -14,22 +14,22 @@ import Database.Persist.Sql
 import Database.Persist.Sqlite
 import Database.Persist.Class as PC
 
-create :: (Validatable at) => at -> EitherT ServantErr IO at
+create :: (Validatable at, FromTable at a) => at -> EitherT ServantErr IO a
 create new = do
   validate new
   runSqlite sqliteFile $ do
     entkey <- insert new
     ent <- get entkey
     case ent of
-      Just n -> return n
+      Just n -> return $ fromTable n (fromSqlKey entkey)
 
-read :: (Validatable at) => Key at -> EitherT ServantErr IO at
+read :: (Validatable at, FromTable at a) => Key at -> EitherT ServantErr IO a
 read key = do
   runSqlite sqliteFile $ do
     ent <- get key
     case ent of
       Nothing -> lift $ lift $ lift $ left err404
-      Just o -> return o
+      Just o -> return $ fromTable o (fromSqlKey key)
 
 update :: (Validatable at) => Key at -> at -> EitherT ServantErr IO ()
 update key new = do
