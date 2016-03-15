fbb804cd75aa336b7bf8704676237316c946027a
octaltree <octaltree@gmail.com>
2016-03-15 13:34:30 +0900

    app's handler use sql defined at models

--- metaserver/src/App.hs
+++ metaserver/src/App.hs
@@ -63,82 +63,43 @@ serverWriter = handlerGetAllWriters
 serverTag :: Server TagEP
 serverTag = handlerGetAllTags
 
-handlerGetAllBooks :: EitherT ServantErr IO [Book]
-handlerGetAllBooks = undefined
-
 handlerPostBook :: Book -> EitherT ServantErr IO Book
-handlerPostBook bk = runSqlite sqliteFile $ do
-  entkey <- insert bk
-  ent <- get entkey
-  case ent of
-    Just b -> return b
-    Nothing -> lift $ lift $ lift (left err404 :: EitherT ServantErr IO Book)
-
+handlerPostBook = createBook
 handlerGetBook :: Int -> EitherT ServantErr IO Book
-handlerGetBook idx = runSqlite sqliteFile $ do
-  bk <- selectFirst [BookId ==. (BookKey $ SqlBackendKey $ fromIntegral idx)] []
-  case bk of
-    Just b -> return $ entityVal b
-    Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO Book)
-
+handlerGetBook = readBook
 handlerPutBook :: Int -> Book -> EitherT ServantErr IO ()
-handlerPutBook idx bk = runSqlite sqliteFile $ do
-  mb <- selectFirst [BookId ==. (BookKey $ SqlBackendKey $ fromIntegral idx)] []
-  case mb of
-    Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO ())
-    Just b -> replace (entityKey b) bk
-
+handlerPutBook = updateBook
 handlerDeleteBook :: Int -> EitherT ServantErr IO ()
-handlerDeleteBook idx = undefined
-
-handlerGetAllCircles :: EitherT ServantErr IO [Circle]
-handlerGetAllCircles = undefined
+handlerDeleteBook = deleteBook
 
 handlerPostCircle :: Circle -> EitherT ServantErr IO Circle
-handlerPostCircle cr = runSqlite sqliteFile $ do
-  entkey <- insert cr
-  ent <- get entkey
-  case ent of
-    Just c -> return c
-    Nothing -> lift $ lift $ lift (left err404 :: EitherT ServantErr IO Circle)
-
+handlerPostCircle = createCircle
 handlerGetCircle :: Int -> EitherT ServantErr IO Circle
-handlerGetCircle idx = runSqlite sqliteFile $ do
-  cr <- selectFirst [CircleId ==. (CircleKey $ SqlBackendKey $ fromIntegral idx)] []
-  case cr of
-    Just c -> return $ entityVal c
-    Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO Circle)
-
+handlerGetCircle = readCircle
 handlerPutCircle :: Int -> Circle -> EitherT ServantErr IO ()
-handlerPutCircle idx cr = runSqlite sqliteFile $ do
-  mc <- selectFirst [CircleId ==. (CircleKey $ SqlBackendKey $ fromIntegral idx)] []
-  case mc of
-    Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO ())
-    Just c -> replace (entityKey c) cr
-
+handlerPutCircle = updateCircle
 handlerDeleteCircle :: Int -> EitherT ServantErr IO ()
-handlerDeleteCircle idx = undefined
-
-handlerGetAllWriters :: EitherT ServantErr IO [Writer]
-handlerGetAllWriters = undefined
+handlerDeleteCircle = deleteCircle
 
 handlerPostWriter :: Writer -> EitherT ServantErr IO Writer
-handlerPostWriter wr = undefined
-
+handlerPostWriter = createWriter
 handlerGetWriter :: Int -> EitherT ServantErr IO Writer
-handlerGetWriter idx = runSqlite sqliteFile $ do
-  wr <- selectFirst [WriterId ==. (WriterKey $ SqlBackendKey $ fromIntegral idx)] []
-  case wr of
-    Just w -> return $ entityVal w
-    Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO Writer)
-
+handlerGetWriter = readWriter
 handlerPutWriter :: Int -> Writer -> EitherT ServantErr IO ()
-handlerPutWriter idx wr = undefined
-
+handlerPutWriter = updateWriter
 handlerDeleteWriter :: Int -> EitherT ServantErr IO ()
-handlerDeleteWriter idx = undefined
+handlerDeleteWriter = deleteWriter
 
 handlerGetAllTags :: EitherT ServantErr IO [String]
 handlerGetAllTags = runSqlite sqliteFile $ do
   bks <- selectList ([] :: [Filter Book]) []
   return $ nub $ concat $ map (bookTags . entityVal) bks
+
+handlerGetAllWriters :: EitherT ServantErr IO [Writer]
+handlerGetAllWriters = undefined
+
+handlerGetAllCircles :: EitherT ServantErr IO [Circle]
+handlerGetAllCircles = undefined
+
+handlerGetAllBooks :: EitherT ServantErr IO [Book]
+handlerGetAllBooks = undefined
