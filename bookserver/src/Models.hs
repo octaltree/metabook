425554fbc716d7de5d65db3198ff20aa8b9628f4
octaltree <octaltree@gmail.com>
2016-03-16 21:57:27 +0900

    template Models.hs

--- bookserver/src/Models.hs
+++ bookserver/src/Models.hs
@@ -1,1 +1,9 @@
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE QuasiQuotes                #-}
 module Models where
+
+-- import Database.Persist
+import Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+|]
