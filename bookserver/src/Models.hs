ea6ae7e8e09d4ca71dacd03d11c7888a6548c275
octaltree <octaltree@gmail.com>
2016-03-16 23:51:06 +0900

    extension

--- bookserver/src/Models.hs
+++ bookserver/src/Models.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Models where
 
 -- import Database.Persist
