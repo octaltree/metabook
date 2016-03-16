cd68ce7e0dddcd2684c90724fc20f3e9875bc8e9
octaltree <octaltree@gmail.com>
2016-03-16 21:50:35 +0900

    template App.hs

--- bookserver/src/App.hs
+++ bookserver/src/App.hs
@@ -1,1 +1,14 @@
-module App where
+module App
+  ( startApp
+  ) where
+
+import Network.Wai (Application)
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.Cors (simpleCors)
+
+startApp :: IO ()
+startApp = do
+  run 8080 app
+
+app :: Application
+app = simpleCors $ undefined
