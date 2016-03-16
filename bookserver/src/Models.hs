3f49a6068ff51713bc215f389de07905a58e8fe4
octaltree <octaltree@gmail.com>
2016-03-17 01:55:49 +0900

    writer & wrap models

--- bookserver/src/Models.hs
+++ bookserver/src/Models.hs
@@ -4,10 +4,40 @@
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Models where
+module Models
+  ( Table
+  , BookAt
+  , Path
+  )where
 
--- import Database.Persist
+import Data.Int (Int64)
 import Database.Persist.TH
+import Data.Aeson.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+BookAtT
+  bookId Int64
+  at String Int64 Either
+  deriving Show Eq
+PathT
+  path String String Either
+  deriving Show Eq
 |]
+
+data BookAt = BookAt {
+  bookat_id :: Int64,
+  bookat_book_id :: Int64,
+  bookat_at :: Either String Int64
+  } deriving (Show, Eq)
+
+data Path = Path {
+  path_id :: Int64,
+  path_path :: Either String String
+  } deriving (Show, Eq)
+
+$(deriveJSON defaultOptions ''BookAt)
+$(deriveJSON defaultOptions ''Path)
+
+type family Table a :: *
+type instance Table BookAt = BookAtT
+type instance Table Path = PathT
