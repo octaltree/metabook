91d507bce59ed8b31f06f873026a5bbde059b114
octaltree <octaltree@gmail.com>
2016-03-17 18:35:48 +0900

    read, update

--- bookserver/src/RunDb.hs
+++ bookserver/src/RunDb.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 module RunDb where
 
@@ -10,6 +11,7 @@ import Database.Persist.TH
 import Database.Persist.Sql
 import Database.Persist.Sqlite
 import Database.Persist.Class as PC
+import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Either (EitherT, left, runEitherT)
 
 create :: (TableWrapper a, Validatable (Table a)) => (Table a) -> EitherT ServantErr IO a
@@ -20,3 +22,25 @@ create new = do
     ent <- get entkey
     case ent of
       Just n -> return $ fromTable n (fromSqlKey entkey)
+
+read :: (TableWrapper a, Validatable (Table a)) => Key (Table a) -> EitherT ServantErr IO a
+read key = do
+  runSqlite sqliteFile $ do
+    ent <- get key
+    case ent of
+      Nothing -> lift $ lift $ lift $ left err404
+      Just o -> return $ fromTable o (fromSqlKey key)
+
+update :: (TableWrapper a, Validatable (Table a)) => Key (Table a) -> (Table a) -> EitherT ServantErr IO ()
+update key new = do
+  validate new
+  runSqlite sqliteFile $ do
+    ent <- get key
+    case ent of
+      Nothing -> lift $ lift $ lift $ (left err404 :: EitherT ServantErr IO ())
+      Just o -> replace key new
+
+delete :: (TableWrapper a, Validatable (Table a)) => Key (Table a) -> EitherT ServantErr IO ()
+delete key = do
+  -- foreignStrict key
+  runSqlite sqliteFile $ PC.delete key
