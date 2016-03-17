6df8a2cef31298ba271fe64f9a21ec765004f25d
octaltree <octaltree@gmail.com>
2016-03-17 10:41:18 +0900

    maybe key, add postat

--- bookserver/src/Models.hs
+++ bookserver/src/Models.hs
@@ -18,11 +18,13 @@ import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sql
 import Data.Aeson.TH
+import Data.Time
 
 $(share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 BookAtT
   bookId Int64
   at Int64
+  postAt UTCTime default=CURRENT_TIME
   UniqueBookAtAt at
   deriving Show Eq
 AtT
@@ -32,13 +34,14 @@ AtT
 |])
 
 data BookAt = BookAt {
-  bookat_id :: Int64,
+  bookat_id :: Maybe Int64,
   bookat_book_id :: Int64,
-  bookat_at :: Int64
+  bookat_at :: Int64,
+  bookat_post_at :: Maybe UTCTime
   } deriving (Show, Eq)
 
 data At = At {
-  at_id :: Int64,
+  at_id :: Maybe Int64,
   at_description :: Description,
   at_path :: String
   } deriving (Show, Eq)
@@ -51,31 +54,33 @@ type instance Table BookAt = BookAtT
 type instance Table At = AtT
 
 class (PersistEntity (Table a), ToBackendKey SqlBackend (Table a)) => TableWrapper a where
-  key :: a -> Int64
-  toTable :: a -> Table a
+  key :: a -> Maybe Int64
+  toTable :: a -> IO (Table a)
   fromTable :: Table a -> Int64 -> a
 
-  toEntity :: a -> Entity (Table a)
-  toEntity x = Entity (toSqlKey $ key x) (toTable x)
   fromEntity :: Entity (Table a) -> a
   fromEntity x = fromTable (entityVal x) (fromSqlKey $ entityKey x)
 
 instance TableWrapper BookAt where
   key = bookat_id
-  toTable x = BookAtT {
-    bookAtTBookId = bookat_book_id x,
-    bookAtTAt = bookat_at x}
+  toTable x = do
+    now <- getCurrentTime
+    return BookAtT {
+      bookAtTBookId = bookat_book_id x,
+      bookAtTAt = bookat_at x,
+      bookAtTPostAt = now}
   fromTable x i = BookAt {
-    bookat_id = i,
+    bookat_id = Just i,
     bookat_book_id = bookAtTBookId x,
-    bookat_at = bookAtTAt x}
+    bookat_at = bookAtTAt x,
+    bookat_post_at = Just $ bookAtTPostAt x}
 
 instance TableWrapper At where
   key = at_id
-  toTable x = AtT {
+  toTable x = return AtT {
     atTDescription = at_description x,
     atTPath = at_path x}
   fromTable x i = At {
-    at_id = i,
+    at_id = Just i,
     at_description = atTDescription x,
     at_path = atTPath x}
